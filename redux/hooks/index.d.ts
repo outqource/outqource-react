a3dc0930158a2280b3816222a7082043bdf7834c
AlpoxDev <alpoxdev@gmail.com>
2022-05-09 01:36:24 +0900

    upgrade: 1.0.1

--- redux/hooks/index.d.ts
+++ redux/hooks/index.d.ts
@@ -1,3 +1,5 @@
 export { default as getUseSlice } from './getUseSlice';
 export { default as useAsyncReducer } from './useAsyncReducer';
 export * from './useAsyncReducer';
+export * from './useAsyncDispatch';
+export * from './useAsyncSelector';
--- redux/hooks/index.js
+++ redux/hooks/index.js
@@ -1,4 +1,6 @@
 export { default as getUseSlice } from './getUseSlice';
 export { default as useAsyncReducer } from './useAsyncReducer';
 export * from './useAsyncReducer';
+export * from './useAsyncDispatch';
+export * from './useAsyncSelector';
 //# sourceMappingURL=index.js.map
