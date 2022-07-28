2e2323b4555b3f1478a0f8cd3e0dc76e3f2198e5
AlpoxDev <alpoxdev@gmail.com>
2022-07-28 09:37:36 +0900

    add: react-query useInfinityQueries

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from './shared';
+export * from './react-query';
+export * from './redux';
+export * from './recoil';
--- src/react-query/index.ts
+++ src/react-query/index.ts
@@ -1,5 +1,6 @@
-import { QueryClient, QueryClientProvider, useQuery, UseQueryOptions, QueryKey, QueryFunction, useMutation } from 'react-query';
+import { QueryClient, QueryClientProvider, useQuery, UseQueryOptions, QueryKey, QueryFunction, useMutation, useInfiniteQuery } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { UseInfiniteQueryOptions } from 'react-query/types/react/types';
 
 export const defaultQueryClientOptions = {
   defaultOptions: { queries: { refetchOnMount: false } },
@@ -17,6 +18,18 @@ export type UseQueryProps<TQueryFnData = unknown, TError = unknown, TData = TQue
   options?: Omit<UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>, 'queryKey' | 'queryFn' | 'initialData'> & { initialData?: () => undefined };
 };
 
+export type CreateUseInfinityQueryProps<TQueryFnData = unknown, TError = unknown, TData = TQueryFnData, TQueryKey extends QueryKey = QueryKey> = {
+  key: ((...args: any[]) => QueryKey) | QueryKey;
+  fn: ((...args: any[]) => QueryFunction) | QueryFunction;
+  options?: Omit<UseInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>, 'queryKey'>;
+};
+
+export type UseInfinityQueryProps<TQueryFnData = unknown, TError = unknown, TData = TQueryFnData, TQueryKey extends QueryKey = QueryKey> = {
+  key?: any;
+  fn?: any;
+  options?: Omit<UseInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>, 'queryKey'>;
+};
+
 export const createUseQueries = (params: CreateUseQueryProps[]) =>
   params.map(param => {
     return <TQueryFnData = unknown, TError = unknown, TData = TQueryFnData, TQueryKey extends QueryKey = QueryKey>(props?: UseQueryProps<TQueryFnData, TError, TData, TQueryKey>) => {
@@ -32,6 +45,24 @@ export const createUseQueries = (params: CreateUseQueryProps[]) =>
       } as Omit<UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>, 'queryKey' | 'queryFn'>;
 
       return useQuery<TQueryFnData, TError, TData, TQueryKey>(queryKey, queryFn, queryOptions);
+    };
+  });
+
+export const createUseInfinityQueries = (params: CreateUseInfinityQueryProps[]) =>
+  params.map(param => {
+    return <TQueryFnData = unknown, TError = unknown, TData = TQueryFnData, TQueryKey extends QueryKey = QueryKey>(props?: UseInfinityQueryProps) => {
+      if (!props) props = {};
+
+      const queryKey = (typeof param.key !== 'function' ? param.key : param.key(props.key)) as TQueryKey;
+
+      const queryFn = (props.fn ? (param.fn as (props?: any) => QueryFunction)(props.fn) : param.fn) as QueryFunction<TQueryFnData, TQueryKey>;
+
+      const queryOptions = {
+        ...(param.options || {}),
+        ...(props.options || {}),
+      } as Omit<UseInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>, 'queryKey'>;
+
+      return useInfiniteQuery<TQueryFnData, TError, TData, TQueryKey>(queryKey, queryFn, queryOptions);
     };
   });
 
